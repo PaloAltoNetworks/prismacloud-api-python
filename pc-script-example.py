c515878e690a587000c0f8e5c1d2c76d6533a226
tkishel <tkishel@gmail.com>
2021-04-30 11:18:50 -0700

    minimize example

--- pc-script-example.py
+++ pc-script-example.py
@@ -1,25 +1,16 @@
-from __future__ import print_function
-try:
-    input = raw_input
-except NameError:
-    pass
 from pc_lib_api import pc_api
 import pc_lib_general
 
 # --Configuration-- #
 
 parser = pc_lib_general.pc_arg_parser_defaults()
-# INSERT SCRIPT-SPECIFIC ARGUMENTS HERE
 args = parser.parse_args()
 
 # --Initialize-- #
 
-pc_lib_general.prompt_for_verification_to_continue(args)
-pc_settings = pc_lib_general.pc_settings_get(args)
-pc_api.configure(pc_settings)
+pc_api.configure(pc_lib_general.pc_settings_get(args))
 
 # --Main-- #
 
-# INSERT SCRIPT CODE HERE
 result = pc_api.current_user()
 print(result)
