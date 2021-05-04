6a35591e8f8719b615387868f19c7ede5b84a688
tkishel <tkishel@gmail.com>
2021-05-04 08:27:33 -0700

    maintenance: quotes and formatting

--- pc_lib/config_helper.py
+++ pc_lib/config_helper.py
@@ -1,18 +1,38 @@
-import os
 import yaml
+import os
 
 class ConfigHelper(object):
     def __init__(self):
-        config = self.read_yml('configs')
-        self.rl_user = config["prisma_cloud"]["username"]
-        self.rl_pass = config["prisma_cloud"]["password"]
-        self.rl_cust = config["prisma_cloud"]["customer_name"]
-        self.rl_api_base = config["prisma_cloud"]["api_base"]
-        self.rl_ca_bundle = config["prisma_cloud"]["ca_bundle"]
-        self.rl_file_name = config["prisma_cloud"]["filename"]
+        config = self.read_yml()
+        self.rl_user      = config['prisma_cloud']['username']
+        self.rl_pass      = config['prisma_cloud']['password']
+        self.rl_cust      = config['prisma_cloud']['customer_name']
+        self.rl_api_base  = config['prisma_cloud']['api_base']
+        self.rl_ca_bundle = config['prisma_cloud']['ca_bundle']
+        self.rl_file_name = config['prisma_cloud']['filename']
 
     @classmethod
-    def read_yml(self, f):
-        yml_path = os.path.join(os.path.dirname(__file__), "../config/%s.yml" % f)
-        with open(yml_path,'r') as stream:
+    def read_yml(self, f='configs.yml', d=None):
+        if not d:
+            d = os.path.join(os.getcwd(), 'config'
+        yaml_file = os.path.join('%s/%s' % d, f)
+        with open(yaml_file, 'r') as stream:
             return yaml.safe_load(stream)
+
+    @classmethod
+    def write_yml(self, config=None, f='configs.yml', d=None):
+        if not config:
+            config = {'prisma_cloud': {}}
+            config['prisma_cloud']['username']      = ''
+            config['prisma_cloud']['password']      = ''
+            config['prisma_cloud']['customer_name'] = ''
+            config['prisma_cloud']['api_base']      = ''
+            config['prisma_cloud']['ca_bundle']     = ''
+            config['prisma_cloud']['filename']      = ''
+        if not d:
+            d = os.path.join(os.getcwd(), 'config'
+        yaml_file = os.path.join('%s/%s' % d, f)
+        with open(yaml_file, 'w') as stream:
+            yaml.dump(config, stream)
+        return None
+
--- pc_lib/redlock_sdk.py
+++ pc_lib/redlock_sdk.py
@@ -8,33 +8,32 @@ class RLSession(object):
     # Maximum number of retries, for any reason.
     max_retries = 5
     # Always retry on these statuses, within the requests session.
-    # Also retry for authentication failure (401) within try_wrapper().
     retry_statuses = [429, 500, 502, 503, 504]
+    # Also retry for authentication failure (401) ... defined within try_wrapper().
 
-    def __init__(self, userid, userpass, customer_name, api_base, ca_bundle):
-        self.api_id = userid
-        self.api_pass = userpass
-        self.cust_name = customer_name
+    def __init__(self, username, password, customer_name, api_base, ca_bundle):
+        self.api_id       = username
+        self.api_pass     = password
+        self.cust_name    = customer_name
         self.api_base_url = api_base
-        self.ca_bundle = ca_bundle
-        self.auth_token = None
+        self.ca_bundle    = ca_bundle
+        self.auth_token   = None
         self.build_client()
         return None
 
     def build_client(self):
-        # Build client object for class instantiation.
         self.client = requests.Session()
         # GlobalProtect generates 'ignore self signed certificate in certificate chain' errors.
         # Resolve by using a valid CA bundle including the 'Palo Alto Networks Inc Root CA' used by GlobalProtect.
         # Hint: Copy the bundle provided by the certifi module (locate via 'python -m certifi') and append the 'Palo Alto Networks Inc Root CA'
         # Options:
-        #   1. Set 'ca_bundle' in 'configs.yml' to a valid CA bundle.
+        #   1. Set 'ca_bundle' in the configuration file to a valid CA bundle.
         #   2. Set 'REQUESTS_CA_BUNDLE' to a valid CA bundle.
         if self.ca_bundle:
             self.client.verify = self.ca_bundle
         self.retries = Retry(total=self.max_retries, status_forcelist=self.retry_statuses, backoff_factor=1)
         self.redlock_http_adapter = HTTPAdapter(pool_connections=1, pool_maxsize=10, max_retries=self.retries)
-        self.session_mount = "https://"
+        self.session_mount = 'https://'
         self.client.mount(self.session_mount, self.redlock_http_adapter)
         return None
 
@@ -43,21 +42,21 @@ class RLSession(object):
         resp = self.client.post(endpoint, json=body)
         if resp.status_code == 200:
             auth_resp_json = resp.json()
-            token = auth_resp_json["token"]
+            token = auth_resp_json['token']
         if resp.status_code == 401:
-            token = "BAD"
+            token = 'BAD'
         return token
 
     def authenticate_client(self):
         success = False
-        prefix = "https://"  + self.api_base_url
-        endpoint = prefix + "/login"
-        body = {"username":self.api_id,"password":self.api_pass,"customerName":self.cust_name}
+        prefix = 'https://'  + self.api_base_url
+        endpoint = prefix + '/login'
+        body = {'username': self.api_id, 'password': self.api_pass, 'customerName': self.cust_name}
         max_tries = 5
         for _ in range(max_tries):
             token = self.get_auth_token(endpoint, body)
-            if token == "BAD":
-                print("Invalid credentials - can not obtain session token.")
+            if token == 'BAD':
+                print('Invalid credentials, cannot obtain an API token.')
             if token is not None:
                 self.auth_token = token
                 success = True
@@ -68,12 +67,10 @@ class RLSession(object):
         return success
 
     def build_header(self):
-        # Construct the authentication header, required for all API requests.
-        header = {"x-redlock-auth": self.auth_token, "Content-Type": "application/json"}
-        return header
+        return {'x-redlock-auth': self.auth_token, 'Content-Type': 'application/json'}
 
     def interact(self, verb, endpoint, params=None, reqbody=None):
-        url = "%s%s" % (self.build_endpoint_prefix(), endpoint)
+        url = '%s%s' % (self.build_endpoint_prefix(), endpoint)
         success = False
         # Authenticated the session now, if necessary.
         if self.auth_token is None:
@@ -88,10 +85,11 @@ class RLSession(object):
             'get':    self.client.get,
             'post':   self.client.post,
             'put':    self.client.put,
+            'patch':  self.client.patch,
             'delete': self.client.delete}
-        # Raise ValueError if invalid action is specified.
+        # Raise ValueError if invalid action/verb is specified.
         if verb not in verb_mapping:
-            raise ValueError("Invalid HTTP action for API: %s" % verb)
+            raise ValueError('Invalid HTTP action for API: %s' % verb)
         if self.auth_token is None:
             self.authenticate_client()
         success, response, exception = self.get_response(verb_mapping[verb], verb, url, params, reqbody)
@@ -107,4 +105,4 @@ class RLSession(object):
         else:
             response = client_method(url, data=json.dumps(reqbody))
         success, exception = utility.parse_status(url, response.status_code, response.text)
-        return [success, response, exception]     
+        return [success, response, exception]
