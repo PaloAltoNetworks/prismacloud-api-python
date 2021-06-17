4d34c2ff09d8d1c7b582f6411bb9080d45c212c5
Warwick Brown <warwick.brown@anaplan.com>
2021-06-17 11:04:20 +0100

    applied pylint and amended some semantic differences

--- pcs_posture_endpoint_client.py
+++ pcs_posture_endpoint_client.py
@@ -1,9 +1,9 @@
 """ Generic Prisma Cloud API Endpoint Client. """
 
+from sys import exit as sys_exit, stderr, stdout
 from json import dumps as json_dumps
 from os import isatty
 from pc_lib import pc_api, pc_utility
-from sys import stderr, stdout
 
 # --Configuration-- #
 
@@ -42,13 +42,14 @@ parser.add_argument(
 args = parser.parse_args()
 
 if not args.uri_params:
-  args.uri_params = None
+    args.uri_params = None
+
 
 if not args.request_body:
-  args.request_body = None
+    args.request_body = None
 
 if not args.pretty:
-  args.pretty = False
+    args.pretty = False
 
 # --Initialize-- #
 
@@ -57,9 +58,9 @@ pc_api.configure(settings)
 
 # --Main-- #
 
-if args.request_body == None:
+if args.request_body is None:
     request_body_data = None
-    if args.uri_params == None:
+    if args.uri_params is None:
         print('API - Executing HTTP request "%s %s"' % (args.http_method, args.uri_path), file=stderr)
     else:
         print('API - Executing HTTP request "%s %s&%s"' % (args.http_method, args.uri_path, args.uri_params), file=stderr)
@@ -70,34 +71,32 @@ else:
         request_body_data = pc_utility.read_json_file(args.request_body)
         print('API - Import of file data %s completed' % args.request_body, file=stderr)
         if args.uri_params == '':
-            print('API - Executing HTTP request "%s %s" with request body:\n%s' % (args.http_method, args.uri_path, request_body_data), file=stderr) 
+            print('API - Executing HTTP request "%s %s" with request body:\n%s' % (args.http_method, args.uri_path, request_body_data), file=stderr)
         else:
             print('API - Executing HTTP request "%s %s&%s" with request body:\n%s' %  (args.http_method, args.uri_path, args.uri_params, request_body_data), file=stderr)
-    except:
+    except (ValueError, FileNotFoundError):
         print('API - Failed to import file %s' % args.request_body, file=stderr)
-        exit(2)
+        sys_exit(2)
 
 # HUGE WARNING: NO VALIDATION HERE IN THIS EXAMPLE (so that it fits the broadest set of use-cases)
 
 # Prompt for warning if interactive
 if isatty(stdout.fileno()):
-  pc_utility.prompt_for_verification_to_continue(args)
+    pc_utility.prompt_for_verification_to_continue(args)
 
 # Make the HTTP request
 
 try:
     response=pc_api.execute(args.http_method,args.uri_path,query_params=args.uri_params,body_params=request_body_data)
-    if args.response_file == None:
-      print('API - HTTP request response is:', file=stderr)
-      if args.pretty:
-          print(json_dumps(response, indent=4, separators=(', ', ': ')), file=stdout)
-      else:
-          print(json_dumps(response), file=stdout)
+    if args.response_file is None:
+        print('API - HTTP request response is:', file=stderr)
+        if args.pretty:
+            print(json_dumps(response, indent=4, separators=(', ', ': ')), file=stdout)
+        else:
+            print(json_dumps(response), file=stdout)
     else:
-      print('API - HTTP request response stored in file %s' % args.response_file, file=stderr)
-      pc_utility.write_json_file(args.response_file, response, pretty=bool(args.pretty))
-except:
+        print('API - HTTP request response stored in file %s' % args.response_file, file=stderr)
+        pc_utility.write_json_file(args.response_file, response, pretty=bool(args.pretty))
+except Exception: # pylint: disable=W0703
     print('API - HTTP request failed', file=stderr)
-    exit(1)
-
-
+    sys_exit(1)
