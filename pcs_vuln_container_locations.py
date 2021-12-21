fd69108729a7e46807ec9e40314d5ab9d74cdc18
Joe Buhr <joe.buhr@gmail.com>
2021-12-21 12:09:42 -0600

    Added mode option to search across registries.

--- pcs_vuln_container_locations.py
+++ pcs_vuln_container_locations.py
@@ -7,6 +7,12 @@ from pc_lib import pc_api, pc_utility
 
 parser = pc_utility.get_arg_parser()
 parser.add_argument(
+    '--mode',
+    type=str,
+    choices=['registry', 'deployed', 'all'],
+    default='all',
+    help='(Optional) - Report on CI, Registry, Deployed, or all Images.')
+parser.add_argument(
     '--cve',
     type=str,
     help='(Optional) - CVE to filter on.')
@@ -45,32 +51,53 @@ print('Testing Compute API Access ...', end='')
 intelligence = pc_api.statuses_intelligence()
 print(' done.')
 print()
-
+# Monitor > Vulnerabilities/Compliance > Images > Registries
+registry_images = {}
+if args.mode in ['registry', 'all']:
+    print('Getting Registry Images ...')
+    images = pc_api.registry_list_read(args.image_id)
+    optional_print(str(len(images))+' images total')
+    for image in images:
+        image_id = image['_id']
+        vulns = image['vulnerabilities']
+        if not vulns:
+#            optional_print('No vulns for '+image_id)
+            continue
+        keep = False
+        for vuln in vulns:
+            if vuln['cve'] == args.cve:
+                keep = True
+                optional_print('Image '+image_id+' is vulnerable to '+args.cve)
+                break
+        if not keep:
+#        optional_print('Excluding '+image_id)
+            continue
 # Monitor > Vulnerabilities/Compliance > Images > Deployed
 deployed_images = {}
-print('Getting Deployed Images ...')
-images = pc_api.images_list_read(args.image_id)
-optional_print(str(len(images))+' images total')
-for image in images:
-    image_id = image['_id']
-    vulns = image['vulnerabilities']
-    if not vulns:
+if args.mode in ['deployed', 'all']:
+    print('Getting Deployed Images ...')
+    images = pc_api.images_list_read(args.image_id)
+    optional_print(str(len(images))+' images total')
+    for image in images:
+        image_id = image['_id']
+        vulns = image['vulnerabilities']
+        if not vulns:
  #       optional_print('No vulns for '+image_id)
-        continue
-    keep = False
-    for vuln in vulns:
-        if vuln['cve'] == args.cve:
-            keep = True
-            optional_print('Image '+image_id+' is vulnerable to '+args.cve)
-            break
-    if not keep:
+            continue
+        keep = False
+        for vuln in vulns:
+            if vuln['cve'] == args.cve:
+                keep = True
+                optional_print('Image '+image_id+' is vulnerable to '+args.cve)
+                break
+        if not keep:
 #        optional_print('Excluding '+image_id)
-        continue
-    optional_print('Locations for '+image['_id'])
-    containers = pc_api.containers_list_read(image['id'])
-    for container in containers:
-        if 'cluster' in container['info']:
-            optional_print(container['info']['imageName']+': '+container['info']['cluster'])
-        else:
-            optional_print(container['info']['imageName']+': no cluster info. Falling back to hostname: '+ container['hostname'])
-    optional_print()
+            continue
+        optional_print('Locations for '+image['_id'])
+        containers = pc_api.containers_list_read(image['id'])
+        for container in containers:
+            if 'cluster' in container['info']:
+                optional_print(container['info']['imageName']+': '+container['info']['cluster'])
+            else:
+                optional_print(container['info']['imageName']+': no cluster info. Falling back to hostname: '+ container['hostname'])
+        optional_print()
