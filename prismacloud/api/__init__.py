33b4c92c4765e75d67a3d8a404eaf4bbbf2d1ad1
tkishel <tkishel@gmail.com>
2022-04-01 10:46:57 -0700

    correct namespace packaging, update version code, and pylint

--- prismacloud/api/__init__.py
+++ prismacloud/api/__init__.py
@@ -4,9 +4,10 @@ import sys
 
 from .pc_lib_api import PrismaCloudAPI
 from .pc_lib_utility import PrismaCloudUtility
+from .version import version as api_version
 
 __author__  = 'Palo Alto Networks CSE/SE/SA Teams'
-__version__ = '4.0'
+__version__ = api_version
 
 MIN_PYTHON = (3, 6)
 if sys.version_info < MIN_PYTHON:
