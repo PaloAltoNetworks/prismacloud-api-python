7559930bdcac9570ce891b5beb75cd3dad5efceb
Simon Melotte <smelotte@paloaltonetworks.com>
2023-04-28 16:23:23 +0200

    Add fixes for CVEs

--- prismacloud/api/pccs/__init__.py
+++ prismacloud/api/pccs/__init__.py
@@ -9,6 +9,7 @@ from ._fixes           import *
 from ._repositories    import *
 from ._scans           import *
 from ._suppressions    import *
+from ._packages        import *
 
 mixin_classes_as_strings = list(filter(lambda x: x.endswith('PrismaCloudAPIPCCSMixin'), dir()))
 mixin_classes = [getattr(sys.modules[__name__], x) for x in mixin_classes_as_strings]
