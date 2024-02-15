8985b7b1f2666c87c15d52e207f34b83eee4676e
Simon Melotte <smelotte@paloaltonetworks.com>
2024-02-15 16:34:05 +0100

    fix code policies

--- prismacloud/api/pccs/_code_policies.py
+++ prismacloud/api/pccs/_code_policies.py
@@ -5,5 +5,5 @@
 class CodePoliciesPrismaCloudAPIPCCSMixin:
     """ Prisma Cloud Code Security API Code Policies Endpoints Class """
 
-    def suppressions_justifications_list_read(self, policy_id):
+    def code_policies_list_read(self, policy_id):
         return self.execute_code_security('GET', 'code/api/v2/policies/%s' % policy_id)
