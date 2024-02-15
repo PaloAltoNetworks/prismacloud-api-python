413fc376bab4208e14b0157b6a3798400d22c0e6
Simon Melotte <smelotte@paloaltonetworks.com>
2024-02-15 16:27:12 +0100

    add code policies endpoint

--- prismacloud/api/pccs/__init__.py
+++ prismacloud/api/pccs/__init__.py
@@ -10,6 +10,7 @@ from ._repositories    import *
 from ._scans           import *
 from ._suppressions    import *
 from ._packages        import *
+from ._code_policies   import *
 
 mixin_classes_as_strings = list(filter(lambda x: x.endswith('PrismaCloudAPIPCCSMixin'), dir()))
 mixin_classes = [getattr(sys.modules[__name__], x) for x in mixin_classes_as_strings]
