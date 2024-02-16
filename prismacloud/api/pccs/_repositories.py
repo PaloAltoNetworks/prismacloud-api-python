d093640353532ebbb75e825ec4aad9906b262527
Simon Melotte <smelotte@paloaltonetworks.com>
2024-02-16 13:24:08 +0100

    add v2 repo endpoint

--- prismacloud/api/pccs/_repositories.py
+++ prismacloud/api/pccs/_repositories.py
@@ -6,8 +6,7 @@ class RepositoriesPrismaCloudAPIPCCSMixin:
     """ Prisma Cloud Code Security API Repositories Endpoints Class """
 
     def repositories_list_read(self, query_params=None):
-        repositories = self.execute_code_security('GET', 'code/api/v1/repositories', query_params=query_params)
-        return repositories
+        return self.execute_code_security('GET', 'code/api/v1/repositories', query_params=query_params)
 
     def repository_name(self, body_params=None):
         return self.execute_code_security('POST', 'code/api/v1/repositories/query', body_params=body_params)
@@ -17,3 +16,6 @@ class RepositoriesPrismaCloudAPIPCCSMixin:
 
     def repositories_update(self, body_params):
         return self.execute_code_security('POST', 'code/api/v1/repositories', body_params=body_params)
+
+    def repositories_list_read_v2(self, query_params=None):
+        return self.execute_code_security('GET', 'code/api/v2/repositories', query_params=query_params)
